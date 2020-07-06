f290eca4504a1bfc3af7ca2a462ff81c530ac0c0
reihanbay <bung.reihan28@gmail.com>
2020-07-06 23:19:41 +0700

    addSekolahKodingCourse

--- js/number.js
+++ js/number.js
@@ -19,4 +19,32 @@ document.getElementById('name').innerHTML = typeof Number(txtNum)
 document.getElementById('name').innerHTML = typeof txtNum
 document.getElementById('name').innerHTML = parseInt(txtNum) //dibulatkan karna integer bilangan bulat
 
+//Math
+//random(basicnya menjadikan random dari angka 0 ke angka 1)
+var test= Math.random().toFixed(2)+ num;
+document.getElementById('name').innerHTML = test
+//agaar randomnya bebas angka walaupun ada batas maksimal
+var x = 10; // batas maksimlas(range)
+var y = 1; // batas minimal
+
+var random= Math.floor(Math.random() * x ) + y // angka 1 adalah batas angka terkecil
+
+//min
+var min = Math.min(1,2,3);
+document.getElementById('name').innerHTML = min
+
+//max
+var max = Math.max(1,2,3);
+document.getElementById('name').innerHTML = max
+
+//round
+var angka = Math.round(1212.5); // membulatkan angka untuk setiap angka > 5 akan dibulatkan ke atas , <5 sebaliknya
+
+//ceil
+var ceil = Math.ceil(1212.1);//tetap membulatkan angkanya(sekecil apapun) ke atas/membesar . exampleResult: 1213
+
+//floor
+var floor = Math.floor(2212.9);//tetap membulatkan angkanya(sekecil apapun) ke bawah/mengecil . exampleResult: 1212
+
+//
 
--- js/string.js
+++ js/string.js
@@ -9,8 +9,16 @@ var txt = "reihan"
 // indexOf
 out.innerHTML = txt.indexOf('e');
 
+// lastindexOf
+out.innerHTML = txt.lastIndexOf('e'); //mencari indeks yg dicari namun yg keluar ygterakhir kali
+
+// search
+out.innerHTML = txt.search('han'); //mencari indeks yg dicari , bisa huruf bisa kata
+
 //substr
 out.innerHTML = txt.substr(1, 3);  //(indexKe, sampaike)
+//substring
+out.innerHTML = txt.substring(1, 3);  //(indexKe, sampaike)
 
 //replace
 out.innerHTML = txt.replace('rei', 'e'); //(apayang diubah, kataganti)
@@ -20,7 +28,7 @@ out.innerHTML = txt.replace('rei', 'e'); //(apayang diubah, kataganti)
 out.innerHTML = txt.toUpperCase()+txt.toLowerCase()
 
 //charAt
-out.innerHTML = txt.charAt(0) //(index Ke)
+out.innerHTML = txt.charAt(1) //(index Ke)
 
 //split
 var txt = "a,b,c,d"
